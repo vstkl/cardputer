982e34c99927bd011b1a3a8a1a75ac7caf82da88
vstkl <dev@vstkl.xyz>
2026-05-17 23:37:57 +0200

    added dummy app

--- main/main.cpp
+++ main/main.cpp
@@ -41,7 +41,7 @@ extern "C" void app_main(void)
     GetMooncake().installApp(std::make_unique<AppStringIRToolKit>());
     GetMooncake().installApp(std::make_unique<AppLoraChat>());
     GetMooncake().installApp(std::make_unique<AppGPS>());
-    // GetMooncake().installApp(std::make_unique<AppDummy>());
+    GetMooncake().installApp(std::make_unique<AppDummy>());
 
     // Main loop
     audio::set_keyboard_sfx_enable(true);
